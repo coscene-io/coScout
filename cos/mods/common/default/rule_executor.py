b9893475814905376718caa58f670bae5e828ec1
Shuhao Qing <shuhao.qing@coscene.io>
2024-11-22 16:36:09 +0800

    nit

--- cos/mods/common/default/rule_executor.py
+++ cos/mods/common/default/rule_executor.py
@@ -12,24 +12,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import celpy
 import json
 import logging
 from collections import namedtuple
+from collections.abc import Callable
 from datetime import datetime, timedelta
 from functools import partial
 
+from rule_engine.action import Action
+from rule_engine.condition import Condition
+from rule_engine.rule import Rule, validate_rules
 from ruleengine.dsl.base_actions import noop_create_moment
 from ruleengine.dsl.validation.config_validator import validate_config_wrapped
-from ruleengine.engine import Engine
+from ruleengine.engine import Rule as V1Rule
 
 from cos.core.api import ApiClient
 from cos.mods.common.default.remote_rule import RemoteRule
 
 _log = logging.getLogger(__name__)
 
+RuleDataItem = namedtuple("RuleDataItem", "topic msg ts msgtype")
+LogMessageDataItem = namedtuple("LogMessageDataItem", "message")
+
+
+def v2_spec_to_rules(project_rules_spec: dict, upload_fn: Callable, project_name: str) -> list[Rule]:
+    """Convert a v2 rule spec to a list of Rule objects"""
+    rules = []
+    for rule_idx, rule_spec in enumerate(project_rules_spec.get("rules", [])):
+        conditions: list[Condition] = []
+        for condition_spec in rule_spec.get("conditions", []):
+            if "raw" in condition_spec:
+                conditions.append(Condition(condition_spec["raw"]))
+            elif "structured" in condition_spec:
+                structured_condition = condition_spec["structured"]
+                conditions.append(
+                    Condition(
+                        "{sc_type}({sc_path}) {sc_op} {sc_type}({sc_value})".format(
+                            sc_type=structured_condition["type"],
+                            sc_path=structured_condition["path"],
+                            sc_op=structured_condition["op"],
+                            sc_value=structured_condition["value"],
+                        )
+                    )
+                )
+
+        actions: list[Action] = []
+        for action_spec in rule_spec.get("actions", []):
+            if "upload" in action_spec:
+                actions.append(
+                    Action("upload", action_spec["upload"], partial(upload_fn, rule=rule_spec, project_name=project_name))
+                )
+        if not actions:
+            continue
+
+        topics: list[str] = rule_spec.get("active_topics", [])
+
+        scopes = rule_spec.get("scopes", [])
+        if not scopes:
+            scopes = [{}]
+        for scope in scopes:
+            rules.append(Rule(rule_spec, conditions, actions, scope, topics, rule_idx, {"project_name": project_name}))
+    return rules
+
 
 def build_engine_from_config(configs, upload_fn=None, api_client: ApiClient = None):
-    rule_list = []
+    v1_rule_list = []
+    v2_rule_list = []
+    active_topics = set()
     for project_rule_sets in configs:
         if not project_rule_sets.get("name", "").endswith("/diagnosisRule"):
             _log.warning("==> Found an invalid project rule set, skipping")
@@ -38,26 +88,114 @@ def build_engine_from_config(configs, upload_fn=None, api_client: ApiClient = No
         for project_rule_set in project_rule_sets["rules"]:
             if not project_rule_set.get("enabled", False):
                 continue
-            validation_result, rules = validate_config_wrapped(
-                project_rule_set,
-                {
-                    "upload": lambda rule: partial(upload_fn, project_name=project_name, rule=rule),
-                    "create_moment": lambda _: noop_create_moment,
-                },
-                project_name,
-            )
-            if not validation_result["success"]:
-                _log.error(
-                    f"==> Failed to build rule for {project_name} "
-                    f"{json.dumps(project_rule_set, indent=2, ensure_ascii=False)} "
-                    f"due to {json.dumps(validation_result, indent=2, ensure_ascii=False)}, skipping"
+
+            if project_rule_set.get("version", "") == "v1":
+                validation_result, rules = validate_config_wrapped(
+                    project_rule_set,
+                    {
+                        "upload": lambda rule: partial(upload_fn, project_name=project_name, rule=rule),
+                        "create_moment": lambda _: noop_create_moment,
+                    },
+                    project_name,
                 )
-                continue
-            rule_list += rules
+                if not validation_result["success"]:
+                    _log.error(
+                        f"==> Failed to build rule for {project_name} "
+                        f"{json.dumps(project_rule_set, indent=2, ensure_ascii=False)} "
+                        f"due to {json.dumps(validation_result, indent=2, ensure_ascii=False)}, skipping"
+                    )
+                    continue
+                v1_rule_list += rules
+                active_topics.update(project_rule_set.get("active_topics", []))
+            elif project_rule_set.get("version", "") == "v2":
+                rules = v2_spec_to_rules(project_rule_set, upload_fn, project_name)
+                validation_result = validate_rules(rules)
+                if not validation_result["success"]:
+                    _log.error(
+                        f"==> Failed to build rule for {project_name} "
+                        f"{json.dumps(project_rule_set, indent=2, ensure_ascii=False)} "
+                        f"due to {json.dumps(validation_result, indent=2, ensure_ascii=False)}, skipping"
+                    )
+                    continue
+                v2_rule_list += rules
+                active_topics.update(project_rule_set.get("active_topics", []))
 
     device = api_client.state.load_state().device.get("name", "")
 
-    def should_trigger_action(proj_name, rule_spec, hit):
+    return CompatibleEngine(v1_rule_list, v2_rule_list, active_topics, api_client, device)
+
+
+class CompatibleEngine:
+    def __init__(
+        self, v1_rules: list[V1Rule], v2_rules: list[Rule], active_topics: set[str], api_client: ApiClient, device: str
+    ):
+        self.v1_rules = v1_rules
+        self.v2_rules = v2_rules
+        self.active_topics = active_topics
+        self.api_client = api_client
+        self.device = device
+
+    def consume_next(self, item: RuleDataItem):
+        """
+        Consume a message and evaluate upon all rules
+        msg_fn: a factory function that returns a message using msg_fn()
+        """
+        if item.topic not in self.active_topics:
+            return
+
+        activation_without_scope = {
+            "msg": celpy.adapter.json_to_cel(item.msg),
+            "topic": celpy.celtypes.StringType(item.topic),
+            "ts": celpy.celtypes.DoubleType(item.ts),
+        }
+
+        for rule in self.v1_rules:
+            triggered_condition_indices = []
+            triggered_scope = None
+
+            for i, cond in enumerate(rule.conditions):
+                res, scope = cond.evaluate_condition_at(item, rule.initial_scope)
+                _log.debug(f"evaluate condition, result: {res}, scope: {scope}")
+                if res:
+                    triggered_condition_indices.append(i)
+                if not triggered_scope:
+                    triggered_scope = scope
+
+            if not triggered_condition_indices:
+                continue
+
+            # For testing, rule.spec is not specified
+            hit = (
+                {}
+                if not rule.spec
+                else {
+                    **rule.spec,
+                    "when": [rule.spec["when"][i] for i in triggered_condition_indices],
+                }
+            )
+
+            should_upload = self.should_upload(rule.project_name, rule.spec, hit)
+            if should_upload:
+                for action in rule.actions:
+                    action.run(item, triggered_scope)
+
+            self.hit_upload(rule.project_name, rule.spec, hit, should_upload)
+
+        for rule in self.v2_rules:
+            if item.topic not in rule.topics:
+                continue
+            activation = {**activation_without_scope, "scope": rule.scope}
+            if not all(cond.evaluate(activation) for cond in rule.conditions):
+                continue
+
+            should_upload = self.should_upload(rule.metadata["project_name"], rule.raw, rule.raw)
+            if should_upload:
+                for action in rule.actions:
+                    action.run(activation)
+
+            self.hit_upload(rule.metadata["project_name"], rule.raw, rule.raw, should_upload)
+
+    def should_upload(self, proj_name, rule_spec, hit):
         if not hit.get("uploadLimit", ""):
             return True
 
@@ -73,7 +211,7 @@ def build_engine_from_config(configs, upload_fn=None, api_client: ApiClient = No
         upload_limit = hit["uploadLimit"]
         if upload_limit.get("device", ""):
             try:
-                device_count = api_client.count_diagnosis_rules_hit(project_rule_spec["name"], hit, device)["count"]
+                device_count = self.api_client.count_diagnosis_rules_hit(project_rule_spec["name"], hit, self.device)["count"]
             except Exception:
                 _log.warning(f"==> Failed to count device hit for {project_rule_spec['name']}, skipping")
                 return False
@@ -85,7 +223,7 @@ def build_engine_from_config(configs, upload_fn=None, api_client: ApiClient = No
 
         if upload_limit.get("global", ""):
             try:
-                global_count = api_client.count_diagnosis_rules_hit(project_rule_spec["name"], hit, "")["count"]
+                global_count = self.api_client.count_diagnosis_rules_hit(project_rule_spec["name"], hit, "")["count"]
             except Exception:
                 _log.warning(f"==> Failed to count global hit for {project_rule_spec['name']}, skipping")
                 return False
@@ -97,7 +235,7 @@ def build_engine_from_config(configs, upload_fn=None, api_client: ApiClient = No
 
         return True
 
-    def trigger_cb(proj_name, rule_spec, hit, action_triggered, _):
+    def hit_upload(self, proj_name, rule_spec, hit, upload_triggered):
         project_rule_spec = {
             "name": f"{proj_name}/diagnosisRule",
             "rules": [
@@ -107,12 +245,10 @@ def build_engine_from_config(configs, upload_fn=None, api_client: ApiClient = No
             ],
         }
         try:
-            api_client.hit_diagnosis_rule(project_rule_spec, hit, device, action_triggered)
+            self.api_client.hit_diagnosis_rule(project_rule_spec, hit, self.device, upload_triggered)
         except Exception:
             _log.warning(f"==> Failed to hit diagnosis rule for {project_rule_spec['name']}, skipping")
             pass
-
-    return Engine(rule_list, should_trigger_action, trigger_cb)
 
 
 class RuleExecutor:
@@ -123,7 +259,7 @@ class RuleExecutor:
         self.__input_stream = input_stream
         self.__upload_fn = upload_fn
         self.__configs = None
-        self.__engine = None
+        self.__engine: CompatibleEngine | None = None
         self.update_config()
 
     def consume_chunk(self):
@@ -150,7 +286,3 @@ class RuleExecutor:
 
     def execute(self):
         self.consume_chunk()
-
-
-RuleDataItem = namedtuple("RuleDataItem", "topic msg ts msgtype")
-LogMessageDataItem = namedtuple("LogMessageDataItem", "message")
