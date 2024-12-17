5cde1c2f839a47f48fb9ec8098bdbc22b2d7a43d
Shuhao Qing <shuhao.qing@coscene.io>
2024-12-17 11:42:33 +0800

    nit

--- cos/mods/common/default/mod.py
+++ cos/mods/common/default/mod.py
@@ -66,7 +66,9 @@ class DefaultMod(Mod):
         self.log_thread_name = "cos-mod-default-log-listener"
         self.task_thread_name = "cos-mod-task-collector-handler"
         self.file_listener_thread_name = "cos-mod-default-file-listener"
-        self.file_state_handler = FileStateHandler.get_instance(self.conf.skip_period_hours, self.conf.ros2_customized_msgs_dirs)
+        self.file_state_handler = FileStateHandler.get_instance(
+            self.conf.skip_period_hours, self.conf.ros2_customized_msgs_dirs
+        )
 
         self.state_dir = DEFAULT_MOD_STATE_DIR
         self.state_dir.mkdir(parents=True, exist_ok=True)
