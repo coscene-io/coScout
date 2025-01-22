f5b8fdd47ba59113e1c073cea37d3e31550ef1db
yuejin.lan <yuejin.lan@coscene.io>
2025-01-22 14:33:19 +0800

    fix: header

--- internal/mod/rule/file_state_handler/file_state_handler.go
+++ internal/mod/rule/file_state_handler/file_state_handler.go
@@ -1,4 +1,4 @@
-// Copyright 2024 coScene
+// Copyright 2025 coScene
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
