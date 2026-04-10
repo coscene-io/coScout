2e3d98caa2bed1516c2dddb86aac60964f9c58a7
renyijiu <me@renyijiu.com>
2026-04-10 14:38:05 +0800

    fix: copyright

--- pkg/upload/cache_key_test.go
+++ pkg/upload/cache_key_test.go
@@ -1,3 +1,17 @@
+// Copyright 2026 coScene
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package upload
 
 import "testing"
@@ -31,4 +45,3 @@ func TestLegacyMultipartCacheKeyRemainsPathBased(t *testing.T) {
 		t.Fatalf("expected scoped upload cache key to differ from legacy path-only key")
 	}
 }
-
